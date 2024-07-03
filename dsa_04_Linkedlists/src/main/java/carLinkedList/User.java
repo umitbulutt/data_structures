f73fbcaa52885830b7c9ed916fafb9f857dd615a
Umit Bulut <umitbulut1993@hotmail.com>
2024-07-03 12:22:17 -0400

    User class created.

---
+++ dsa_04_Linkedlists/src/main/java/carLinkedList/User.java
@@ -0,0 +1,13 @@
+package carLinkedList;
+
+public class User {
+    int id;
+    String name;
+    User next;
+
+    public User(int id, String name){
+        this.id =id;
+        this.name= name;
+    }
+
+}
